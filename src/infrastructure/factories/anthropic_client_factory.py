621bee4907de48b1c5738591aa480b80ecdb1dd2
alexribeirofaria <alexfariakof@yahoo.com.br>
2026-04-06 17:56:01 -0300

    feat: adicionado suporte ao Anthropic Client

--- src/infrastructure/factories/anthropic_client_factory.py
+++ src/infrastructure/factories/anthropic_client_factory.py
@@ -1,8 +1,7 @@
+from anthropic import Anthropic
 from infrastructure.factories.abstracts.base_client_factory import BaseClientFactory
 
 class AnthropicClientFactory(BaseClientFactory):
-    def _build_client(self):
-        from anthropic import Anthropic
-
-        api_key = self.get_required_env("LLM_CLAUDE_API_KEY", "Claude API key não encontrada no .env")
+    def _build_client(self) -> Anthropic:
+        api_key: str = self.get_required_env(key="LLM_CLAUDE_API_KEY", error_message="Claude API key não encontrada no .env")
         return Anthropic(api_key=api_key)
