1bd763b4211d9de5efe7ec967aa7f56cc3a65fd5
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2026-04-16 01:50:25 -0300

    refactor: update main.ts to bootstrap with NgModule pattern

--- angular-app/src/main.ts
+++ angular-app/src/main.ts
@@ -1,12 +1,11 @@
-import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
-import { platformBrowser } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { AppModule } from "./app/app.module";
+import { environment } from "../environments/environment";
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowser()
-  .bootstrapModule(AppComponent)
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
