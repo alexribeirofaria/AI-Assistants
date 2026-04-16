9aeee08e88a17e287b5f2a0cae93a3bdc7e3cada
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2026-04-16 01:49:33 -0300

    refactor: convert AppComponent from standalone to NgModule

--- angular-app/src/app/app.component.spec.ts
+++ angular-app/src/app/app.component.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
 
 describe('AppComponent', () => {
   let component: AppComponent;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent]
+      imports: [AppModule]
     });
 
     const fixture = TestBed.createComponent(AppComponent);
--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
-import { CookieConsentComponent } from './shared/components/cookie-consent/cookie-consent.component';
+import { Component } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { CookieConsentComponent } from "./shared/components/cookie-consent/cookie-consent.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: true,
-  providers: [],
-  imports: [CookieConsentComponent, RouterModule],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  standalone: false
 })
 export class AppComponent { }
