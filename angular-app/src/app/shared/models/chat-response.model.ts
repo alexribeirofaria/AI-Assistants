26a10ada40a35945ce53ceca2111b826569ccf81
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2026-04-17 16:42:14 -0300

    feat: add chat response type interfaces

---
+++ angular-app/src/app/shared/models/chat-response.model.ts
@@ -0,0 +1,30 @@
+﻿export interface IProviderListResponse {
+  providers: string[];
+}
+
+export interface IModelResponse {
+  id: string;
+  modelName: string;
+  provider: string;
+}
+
+export interface IModelsListResponse {
+  models: IModelResponse[];
+}
+
+export interface IAssistantResponseData {
+  names?: string[];
+  header?: string;
+  message?: string;
+  model?: string;
+  response?: string;
+}
+
+export interface IAssistantResponse {
+  input: string;
+  response?: IAssistantResponseData;
+}
+
+export interface IChangeProviderResponse {
+  status: string;
+}
