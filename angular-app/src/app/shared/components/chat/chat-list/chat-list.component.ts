d107df70e96c5f6f6805e5c1eb21428c7f5809d3
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2026-04-17 04:07:15 -0300

    feat: update chat list component

--- angular-app/src/app/shared/components/chat/chat-list/chat-list.component.ts
+++ angular-app/src/app/shared/components/chat/chat-list/chat-list.component.ts
@@ -21,9 +21,11 @@ export class ChatListComponent implements AfterViewChecked {
   @Input() messages: IMessage[] = [];
   @ViewChild("scrollContainer")
   private scrollContainer!: ElementRef<HTMLElement>;
+  
   ngAfterViewChecked(): void {
     this.scrollToBottom();
   }
+  
   private scrollToBottom(): void {
     if (this.scrollContainer) {
       const el = this.scrollContainer.nativeElement;
