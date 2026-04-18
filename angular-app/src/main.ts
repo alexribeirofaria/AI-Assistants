b4082714da4b6f87fa696c8a3163571138004367
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2026-04-18 19:27:16 -0300

    chore: update main entry and new eslint config

--- angular-app/src/main.ts
+++ angular-app/src/main.ts
@@ -1,7 +1,8 @@
 import { enableProdMode } from "@angular/core";
+import { platformBrowser } from "@angular/platform-browser";
+
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
-import { platformBrowser } from "@angular/platform-browser";
 
 if (environment.production) {
   enableProdMode();
