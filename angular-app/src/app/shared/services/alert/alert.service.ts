4a9fb272350da0c7a3fb0b627fbfb13f039b19f4
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2026-04-18 18:43:22 -0300

    feat: add alert service

---
+++ angular-app/src/app/shared/services/alert/alert.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { AlertModel } from "../../models";
+
+@Injectable({ providedIn: "root" })
+export class AlertService {
+  private subject = new BehaviorSubject<AlertModel | null>(null);
+  alert$ = this.subject.asObservable();
+
+  show(alert: AlertModel): void {
+    this.subject.next(alert);
+  }
+
+  hide(): void {
+    this.subject.next(null);
+  }
+}
