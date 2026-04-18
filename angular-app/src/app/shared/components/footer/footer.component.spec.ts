fdba1e09d9f34984736dc06244d6dbf2a7a17df2
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2026-04-18 18:42:03 -0300

    refactor: update footer component

--- angular-app/src/app/shared/components/footer/footer.component.spec.ts
+++ angular-app/src/app/shared/components/footer/footer.component.spec.ts
@@ -1,5 +1,4 @@
-
-
+/* tslint:disable:no-unused-variable */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FooterComponent } from './footer.component';
 
--- angular-app/src/app/shared/components/footer/footer.component.ts
+++ angular-app/src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   standalone: true
 })
-export class FooterComponent { }
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  public ngOnInit(): void { }
+}
