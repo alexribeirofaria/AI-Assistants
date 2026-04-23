bff7729abd0081bf88b478d13b584c7b1b7a264d
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2026-04-23 00:07:41 -0300

    refactor: ajusta arquivos legados em snake case do decorator

--- angular-app/src/app/core/application/decorator/strategies/decorator/decorator_helper_strategy.ts
+++
@@ -1,4 +0,0 @@
-// Auto‑generated from python-app\application\decorator\strategies\decorator\decorator_helper_strategy.py
-export class DecoratorHelperStrategy {
-  // TODO: implement conversion logic
-}
