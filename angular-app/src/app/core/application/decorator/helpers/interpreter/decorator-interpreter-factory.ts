b23e3b4cc62abb7d31b895df616f50c1f97e8f42
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2026-04-23 00:07:24 -0300

    refactor: ajusta arquivos legados em snake case do decorator

--- angular-app/src/app/core/application/decorator/helpers/interpreter/decorator-interpreter-factory.ts
+++
@@ -1,9 +0,0 @@
-import { DecoratorInterpreter } from './decorator-interpreter';
-import { DecoratorHelperStrategy } from '../decorator/decorator-helper-strategy';
-
-export class DecoratorInterpreterFactory {
-  static create(): DecoratorInterpreter {
-    const strategies = DecoratorHelperStrategy.createInstances();
-    return new DecoratorInterpreter(strategies);
-  }
-}
