9e550211049db2e7b7c9ca8b1a9909763a73a4e5
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2026-04-23 00:08:33 -0300

    refactor: ajusta strategy de repositório baseada em builder

--- angular-app/src/app/core/infrastructure/repository/strategies/repository-strategy.ts
+++ angular-app/src/app/core/infrastructure/repository/strategies/repository-strategy.ts
@@ -1,19 +1,11 @@
-import { BaseDomain } from '../../../../domain/abstracts/base-domain';
-import { BaseServer } from '../../../servers/abstracts/base-server';
-import { Builder } from '../../builder';
+import { BaseDomain } from '../../../domain';
+import { Builder } from '../builder';
 import { BaseRepositoryStrategy } from './abstracts/base-repository-strategy';
 
 export class RepositoryStrategy implements BaseRepositoryStrategy {
-  constructor(
-    private _domain: BaseDomain,
-    private _builder: Builder
-  ) {}
+  constructor(private readonly builder: Builder) {}
 
   buildDomain(): BaseDomain {
-    return this._domain;
-  }
-
-  get server(): BaseServer {
-    return this._builder.server;
+    return this.builder.buildDomain();
   }
 }
