5aba3e697d9dec274318c53edee17b0e120db628
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2026-04-24 21:21:14 -0300

    chore: ajustar console.app.ts

--- angular-app/src/app/core/console.app.ts
+++
@@ -1,8 +0,0 @@
-import { ConsoleCliService, DefaultRuntimeService } from './app';
-
-const runtime = new DefaultRuntimeService();
-
-/* istanbul ignore next */
-if (runtime.isCliEnvironment) {
-  void new ConsoleCliService().execute({ runtime });
-}
