a46cf633e466abbe09f9b774449af488c5cc539b
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2026-04-24 21:21:41 -0300

    refactor: reorganizar inicialização do app de console

---
+++ angular-app/src/app/core/app-cli-node/console.app.ts
@@ -0,0 +1,7 @@
+import { ConsoleCliService, DefaultRuntimeService } from './';
+
+const runtime = new DefaultRuntimeService();
+
+if (runtime.isCliEnvironment) {
+  void new ConsoleCliService().execute({ runtime });
+}
