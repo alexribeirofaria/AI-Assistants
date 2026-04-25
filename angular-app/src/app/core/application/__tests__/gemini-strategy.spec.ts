4cfe791196e37b59db17da9c4f3040985032fef4
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2026-04-24 21:21:04 -0300

    test: ajustar cobertura em gemini-strategy.spec.ts

--- angular-app/src/app/core/application/__tests__/gemini-strategy.spec.ts
+++ angular-app/src/app/core/application/__tests__/gemini-strategy.spec.ts
@@ -2,6 +2,6 @@ import { GeminiStrategy } from '../strategies/gemini-strategy';
 
 describe('GeminiStrategy', () => {
   it('binds the Gemini domain', () => {
-    expect(new GeminiStrategy().domainClass.name).toBe('GeminiDomain');
+    expect(new GeminiStrategy().domainClass.name).toBe('Gemini');
   });
 });
