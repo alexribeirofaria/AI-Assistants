4a4db7882a257d0e0d2893c6a75ecd5dce6af96c
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2026-04-24 21:21:07 -0300

    test: ajustar cobertura em message-strategy.spec.ts

--- angular-app/src/app/core/application/__tests__/message-strategy.spec.ts
+++ angular-app/src/app/core/application/__tests__/message-strategy.spec.ts
@@ -1,8 +1,20 @@
+import { UserAction } from '../enums/user-action';
 import { MessageStrategy } from '../decorator/helpers/strategies/message-strategy';
 
-describe('MessageStrategy', () => {
-  it('should be instantiated', () => {
-    const instance = new MessageStrategy();
-    expect(instance).toBeTruthy();
+describe('MessageStrategy Unit Tests', () => {
+  let instance: MessageStrategy;
+
+  beforeEach(() => {
+    instance = new MessageStrategy();
+  });
+
+  it('always handles any input as message', () => {
+    expect(instance.canHandle('anything', ['anything'])).toBeTrue();
+    expect(instance.canHandle('', [])).toBeTrue();
+  });
+
+  it('returns message action with tokens joined as payload', () => {
+    expect(instance.handle('ignored', ['hello', 'world'])).toEqual([UserAction.MESSAGE, 'hello world']);
+    expect(instance.handle('ignored', [])).toEqual([UserAction.MESSAGE, '']);
   });
 });
