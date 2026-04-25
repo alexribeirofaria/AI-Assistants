ae828473e069577ae9c6015515868a8ea4bd2f50
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2026-04-24 21:21:36 -0300

    chore: atualizar configuração de ambiente

--- angular-app/src/environments/environment.ts
+++ angular-app/src/environments/environment.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
   BASE_URL: "http://localhost:5000",
+  LLM_CLAUDE_API_KEY: "",
+  LLM__OPEN_API_KEY :"",
+  LLM__GEMINI_API_KEY: "",
+  LLM__GROQ_API_KEY: "",
+  LLM__DEEPSEEK_API_KEY: ""
 };
