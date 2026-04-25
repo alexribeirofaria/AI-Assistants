37c18a95d9eb4304ef900d6634c85105fb913ff0
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2026-04-24 21:21:04 -0300

    test: ajustar cobertura em exit-strategy.spec.ts

--- angular-app/src/app/core/application/__tests__/exit-strategy.spec.ts
+++ angular-app/src/app/core/application/__tests__/exit-strategy.spec.ts
@@ -1,8 +1,24 @@
+import { UserAction } from '../enums/user-action';
 import { ExitStrategy } from '../decorator/helpers/strategies/exit-strategy';
 
-describe('ExitStrategy', () => {
-  it('should be instantiated', () => {
-    const instance = new ExitStrategy();
-    expect(instance).toBeTruthy();
+describe('ExitStrategy Unit Tests', () => {
+  let instance: ExitStrategy;
+
+  beforeEach(() => {
+    instance = new ExitStrategy();
+  });
+
+  it('matches exit aliases', () => {
+    expect(instance.canHandle('exit', ['exit'])).toBeTrue();
+    expect(instance.canHandle('quit', ['quit'])).toBeTrue();
+    expect(instance.canHandle('sair', ['sair'])).toBeTrue();
+  });
+
+  it('does not match unrelated commands', () => {
+    expect(instance.canHandle('models', ['models'])).toBeFalse();
+  });
+
+  it('returns exit action', () => {
+    expect(instance.handle('exit', ['exit'])).toEqual([UserAction.EXIT, null]);
   });
 });
